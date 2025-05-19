e99f90b926ffb853c81f35b16a1ee5b533221c18
alexander.zhuravlev <zhur06@gmail.com>
2025-05-20 01:49:23 +0200

    feat(settings): Fixed cookies set up

---
+++ src/app/unauthorized/page.tsx
@@ -0,0 +1,9 @@
+const Unauthorized = () => {
+  return (
+    <>
+      Welcome to YourNews! To access the site, please authorize using the link from <a href="https://t.me/amogsu2ns2nc28bot" className="underline">telegram bot</a>
+    </>
+  )
+}
+
+export default Unauthorized
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,88 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { setCookie } from '@/lib/cookies';
+
+const USER_ID_IN_COOKIES = 'yournews_user_id';
+const USER_ID_IN_SEARCH_PARAMS = 'user_id';
+
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
   const supabase = createMiddlewareClient({ req, res });
   await supabase.auth.getSession();
 
+  const user_id_in_params = req.nextUrl.searchParams.get(USER_ID_IN_SEARCH_PARAMS);
+  const user_id_in_cookies = req.cookies.get(USER_ID_IN_COOKIES)?.value;
 
-  const user_id = req.nextUrl.searchParams.get('yournews_user_id');
+  const user_id = user_id_in_params || user_id_in_cookies;
   
   if (!user_id) {
-    if (req.cookies.get('yournews_user_id')){
+    // Not to spam redirects
+    if (req.nextUrl.pathname === '/unauthorized')
       return res;
-    }
-    
+
+    // No user_id provided neither in url searchParams nor in cookies
     return NextResponse.redirect(new URL('/unauthorized', req.url));
-  }  
+  }
+
+  // Check user_id validity
+
+  const { data, error } = await supabase
+    .from('users')
+    .select('*')
+    .eq('id', user_id);
   
-  const response = NextResponse.redirect(req.nextUrl.pathname);
-  response.cookies.set("yournews_user_id", user_id);
+  if (error) {
+    console.error('Supabase error:', error);
+    return res;
+  }
+
+  if (!data || data.length === 0) {
+    console.log('User not found: ', user_id);
+    return NextResponse.redirect(new URL('/unauthorized', req.url));
+  }
+
+  if (
+    (req.nextUrl.pathname !== '/unauthorized' && req.nextUrl.pathname !== '/settings')
+    && (!data[0].ai_profile
+    || !data[0].description
+    || !data[0].queries
+    || !data[0].cron_pattern
+    || !data[0].timezone)
+  ) {
+    // The profile is not completed
+    console.log('Profile is not completed: ', user_id, req.nextUrl.pathname, data[0]);
+
+    if (!user_id_in_params) {
+      return NextResponse.redirect(new URL('/settings'));
+    }
+
+    // Delete user_id from searchParams
+    const url = req.nextUrl.clone();
+    url.searchParams.delete('user_id');
+    req.nextUrl.pathname = url.pathname;
+    req.nextUrl.search = url.search;
+
+    const response = NextResponse.redirect(new URL('/settings', req.url));
+    // Update cookies
+    response.cookies.set(USER_ID_IN_COOKIES, user_id);
+    
+    return response;
+  }
+
+  if (!user_id_in_params) {
+    return res;
+  }
+
+  // Delete user_id from searchParams
+  const url = req.nextUrl.clone();
+  url.searchParams.delete('user_id');
+  req.nextUrl.pathname = url.pathname;
+  req.nextUrl.search = url.search;
+
+  const response = NextResponse.redirect(url);
+  // Update cookies
+  response.cookies.set(USER_ID_IN_COOKIES, user_id);
   
   return response;
 }
